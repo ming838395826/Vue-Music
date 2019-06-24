1f2aa020a6c240e5514c731748778964add1d0aa
ming838395826 <838395826@qq.com>
2019-06-24 14:12:17 +0800

    提交接口请求

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -3,7 +3,26 @@
 </template>
 
 <script>
+	import getRecommend from 'api/recommend.js'
+	import {
+		ERR_OK
+	} from 'api/config'
+	export default {
+		created() {
+			_getRecommend();
+		},
+		methods: {
+			_getRecommend() {
+				getRecommend().then((res) => {
+					console.log(res);
+					if (res.code === ERR_OK) {
+						// this.recommends = res.data.slider
+					}
+				})
+			}
+		}
+	}
 </script>
-	
+
 <style>
 </style>
